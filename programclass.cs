862c9041df497ffe98a048a4ca84897c75ae3812
ultron3 <alex.gnavi@gmail.com>
2023-07-15 17:34:04 +0200

    aggiunti nuovi file

---
+++ programclass.cs
@@ -0,0 +1,17 @@
+using System;
+
+    class programclass
+    {
+        private string Hardware {get;set};
+        private string Software{get;set};
+    }
+
+  private void main(string[]arg){
+    console.WriteLine("Enter name hardware: ")
+    string component=Console.Readline(Hardaware);
+    console.WriteLine("Enter name software: ")
+    string os=Console.Readline(Software)
+  }
+   
+
+
