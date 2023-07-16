c4718a4cc1d10c203e79bd107242f18a352ad550
ultron3 <alex.gnavi@gmail.com>
2023-07-16 10:53:02 +0200

    Nel file programclass, codice atm

--- programclass.cs
+++ programclass.cs
@@ -1,17 +1,41 @@
 using System;
 
-    class programclass
+public class ATM
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World");
+     int budget = 10000;
+
+    Console.WriteLine("Inserisci un'opzione:");
+    string opzione = Console.ReadLine();
+
+    switch (opzione)
     {
-        private string Hardware {get;set};
-        private string Software{get;set};
+      case "balance":
+        
+        Console.Writeline("hai un budget  di: " + budget);
+        break;
+
+      case "withdraw":
+       
+        Console.WriteLine("Inserisci l'importo da prelevare:");
+        int money = Convert.ToInt32(Console.ReadLine());
+        int result = budget - money;
+        Console.WriteLine("Hai prelevato: " + money);
+        Console.WriteLine("Ti rimane un budget di: " + result);
+        break;
+      case "deposit":
+       
+        Console.WriteLine("Inserisci il denaro:");
+        int money1 = Convert.ToInt32(Console.ReadLine());
+        int result_deposit = budget + money1;
+        Console.WriteLine("Hai depositato: " + money1);
+        Console.WriteLine("Hai un budget di: " + result_deposit);
+        break;
+      default:
+        Console.WriteLine("Opzione non valida.");
+        break;
     }
-
-  private void main(string[]arg){
-    console.WriteLine("Enter name hardware: ")
-    string component=Console.Readline(Hardaware);
-    console.WriteLine("Enter name software: ")
-    string os=Console.Readline(Software)
   }
-   
-
-
+}
